e44c9a637203989dc492bc2ee34f58c946aa3497
Tsbap <48564739+Tsbap@users.noreply.github.com>
2019-04-27 14:32:25 -0300

    Update WARfile.cpp
    Regras adicionadas

--- WARfile.cpp
+++ WARfile.cpp
@@ -8,22 +8,26 @@
 using namespace std;
 
 void regrasInicio(){
-	cout << "" << endl;
+	cout << "\n\n\t::::::::::::::::::::::::::::::REGRAS::::::::::::::::::::::::::::::" << endl;
+	cout << "\n\t1.No inicio do jogo os 18 paises serao sorteados e divididos\n\t igualmente entre a quantidade de players." << endl;
+	cout << "\n\t2.O vencedor sera decidido pelo que dominar todos os paises." << endl;
+	cout << "\n\t3.Todo inicio de rodada o jogador recebera uma quantidade de\n\t tropas referente a metade do numero de paises dominados." << endl;
+	cout << "\n\t4.O jogador podera apenas atacar quando tiver mais de um exercito\n\t e fizer fronteira com o pais inimigo." << endl;
+	cout << "\n\t5.Durante seu ataque o jogador jogará uma quantidade de dados\n\t referentes ao numero de exercitos -1, sendo o maximo 3 dados." << endl;
+	cout << "\n\t6.Na defesa, o jogador jogara uma quantidade de dados referentes\n\t ao numero de exercitos, sendo o maximo 3 dados." << endl;
+	cout << "\n\n\t\tPressione BACKSPACE para retornar ou ENTER para continuar." << endl;
 }
 
 int numeroPlayers;
 
 int telaInicio(){
 	
-	string str = "\tNumero invalido de players";
-	char aux2;
-	int c;
-	cout<<"\n\n\t\t\tPress space to start..."<<endl;
-	char aux;
-	do{
-		aux=getch();
 	
-	}while(aux!=32);
+	string str = "\tNumero invalido de players";
+	int c;
+	
+	char aux2;
+	
 	
 	system("cls");
 	system("color 02");
@@ -72,13 +76,29 @@ int telaInicio(){
 }
 
 int main(void){
-	telaInicio();
-	Sleep(1000);
 	
+	char aux;
+	cout<<"\n\n\t\t\tPress ENTER to start..."<<endl;
 	
+	do{
+		aux=getch();
 	
+	}while(aux!=13);
 	
+	char aux3=8;
+	
+	while(aux3 ==8  && aux3!=13){
+		system("cls");
+		telaInicio();
+		Sleep(1000);
+		system("cls");
+		regrasInicio();
+		aux3=getch();
+		fflush(stdin);
+		
+	}
+	system("cls");
+	cout << "\thello world" << endl;
 	
 	return 0;
 }
-
